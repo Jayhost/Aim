b5778f26d1d2b49b792aeb65fa97d6e0ce32b666
ian <jadenhorst@gmail.com>
2025-10-09 13:18:22 +0300

    pass vulkan to docker

--- blazor-app/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ blazor-app/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d365897b885d69bbd5be32d34b1e25c1a05d6c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
